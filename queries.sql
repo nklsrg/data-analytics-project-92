673e54d2f67d773ee16fb07204e20b79391792d6
Sergei <119746078+nklsrg@users.noreply.github.com>
2024-10-25 16:07:38 +0300

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -56,3 +56,48 @@ inner join sales as s on s.sales_person_id = e.employee_id
 inner join products as p on p.product_id = s.product_id
 group by e.employee_id, to_char(s.sale_date, 'id')
 order by to_char(s.sale_date, 'id'), seller;
+
+--запрос, который выводит количество покупателей в разных возрастных группах
+select
+    '16-25' as age_category,
+    COUNT(age) as age_count
+from customers as c 
+where age >= 16 and age <=25
+
+UNION 
+
+select
+    '26-40' as age_category,
+    COUNT(age) as age_count
+from customers as c 
+where age >= 26 and age <=40
+
+UNION 
+
+select
+    '40+' as age_category,
+    COUNT(age) as age_count
+from customers as c 
+where age > 40
+order by age_category;
+
+--запрос, который выводит количество уникальных покупателей и выручку, которую они принесли
+select 
+	to_char(s.sale_date, 'yyyy-mm') as selling_month,
+	count(distinct s.customer_id) as total_customes,
+	floor(sum(p.price*s.quantity)) as income
+from sales as s
+inner join products as p on p.product_id = s.product_id
+group by selling_month;
+
+--запрос, который выводит данные о покупателях, первая покупка которых была в ходе проведения акций (акционные товары отпускали со стоимостью равной 0)
+select DISTINCT ON (s.customer_id)
+    (concat_ws(' ', c.first_name, c.last_name)) as customer,
+    s.sale_date,
+    concat_ws(' ', e.first_name, e.last_name) as seller
+from sales as s 
+inner join products as p on p.product_id = s.product_id
+inner join customers as c on s.customer_id = c.customer_id 
+inner join employees as e on s.sales_person_id = e.employee_id 
+where p.price = 0
+order by s.customer_id, s.sale_date;
